d6c0b404d26f974b2880e09b27749e21f7b8ecbf
amyumangulov <amyumangulov@inbox.ru>
2020-07-07 17:38:26 +0600

    проблема с фильтром объекта

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -1,71 +1,71 @@
 <template>
-    <div id="schedules">
-        <h2>{{description}}</h2>
-
-
-        <div class="schedules" v-for="(schedule, key) in schedules" :key="key">
-            <div class="date">
-                <p>{{schedule.date}}</p>
-            </div>
-            <div class="descs">
-                <div v-for="descs in schedule.descs" :key="descs.serviceTime">
-                    <div class="desc">
-                        {{ descs.time }} <a href="#"> {{ descs.desc }} </a>
-                    </div>
-                </div>
-            </div>
-
+  <div id="schedules">
+    <h2>{{description}}</h2>
+    <div class="schedules" v-for="(schedule, key) in schedules" :key="key">
+      <div class="date">
+        <p>{{schedule.date}}</p>
+      </div>
+      <div class="descs">
+        <div v-for="descs in schedule.descs" :key="descs.serviceTime">
+          <div class="desc">
+            <span class="material-icons" @click="findTime(descs.time)">
+              create
+            </span>
+            {{ descs.time }} <a href="#"> {{ descs.desc }} </a>
+          </div>
         </div>
-        <button class="v_catalog-item__show-info" @click="showPopupInfo">
-            Добавить занятие
-        </button>
-        <add_Lesson v-if="isInfoPopupVisible" @closePopup="closeInfoPopup" @AddLesson="addRow" class="popup">
-            <div class="form">
-                <p>Lesson</p>
-                <input type="text" v-model="lesson">
-            </div>
-            <div class="form">
-                <p>Auditor</p>
-                <input type="text" v-model="Auditor">
-            </div>
-            <div class="form">
-                <p>Date</p>
-                <input type="text" v-model="date">
-            </div>
-            <div class="form">
-                <p>Time</p>
-                <input type="text" v-model="time">
-            </div>
-            <div class="form">
-                <p>Description</p>
-                <input type="text" v-model="Description">
-            </div>
-            <div class="form">
-                <p>Repeat</p>
-                <div class="popup__radio-group">
-                    <div class="radio">
-                        <input type="radio" id="age1" name="age" value="every day" v-model="Repeat">
-                        <label for="age1">каждый день</label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" id="age2" name="age" value="every week" v-model="Repeat">
-                        <label for="age2">еженедельно</label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" id="age3" name="age" value="Two times per week" v-model="Repeat">
-                        <label for="age3">2 раза в неделю</label>
-                    </div>
-                </div>
-            </div>
-        </add_Lesson>
+      </div>
 
     </div>
+    <button class="v_catalog-item__show-info" @click="showPopupInfo">
+      Добавить занятие
+    </button>
+    <add_Lesson v-if="isInfoPopupVisible" @closePopup="closeInfoPopup" @AddLesson="addRow" class="popup">
+      <div class="form">
+        <p>Lesson</p>
+        <input type="text" v-model="lesson">
+      </div>
+      <div class="form">
+        <p>Auditor</p>
+        <input type="text" v-model="Auditor">
+      </div>
+      <div class="form">
+        <p>Date</p>
+        <input type="text" v-model="date">
+      </div>
+      <div class="form">
+        <p>Time</p>
+        <input type="text" v-model="time">
+      </div>
+      <div class="form">
+        <p>Description</p>
+        <input type="text" v-model="Description">
+      </div>
+      <div class="form">
+        <p>Repeat</p>
+        <div class="popup__radio-group">
+          <div class="radio">
+            <input type="radio" id="age1" name="age" value="every day" v-model="Repeat">
+            <label for="age1">каждый день</label>
+          </div>
+          <div class="radio">
+            <input type="radio" id="age2" name="age" value="every week" v-model="Repeat">
+            <label for="age2">еженедельно</label>
+          </div>
+          <div class="radio">
+            <input type="radio" id="age3" name="age" value="Two times per week" v-model="Repeat">
+            <label for="age3">2 раза в неделю</label>
+          </div>
+        </div>
+      </div>
+    </add_Lesson>
+
+  </div>
 </template>
 
 
 <script>
     import add_Lesson from '../components/add_Lesson'
-
     export default {
         components: {add_Lesson},
         name: 'schedules',
@@ -124,58 +124,69 @@
                 } else {
                     alert("Необходимо ввести дату!")
                 }
+            },
+            findTime(time) {
+                const ids = time;
+
+                const items = this.schedules.flatMap(n => n.descs).filter(n => ids.includes(n.time));
+                console.log(items);
+
             }
         }
     }
 </script>
 
 <style>
-    .popup {
-        display: flex;
-        flex-direction: column;
+  .popup {
+    display: flex;
+    flex-direction: column;
 
-    }
+  }
 
-    .form {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        margin-top: 10px;
+  .form {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 10px;
 
-    }
+  }
 
-    .popup__radio-group {
-        display: flex;
-        flex-direction: column;
-        width: 40%;
-        margin-bottom: 10px;
-    }
+  .material-icons {
+    cursor: pointer;
+  }
 
-    .schedules {
-        display: flex;
-        flex-direction: row;
-        /*border: aqua solid 1px;*/
-    }
+  .popup__radio-group {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin-bottom: 10px;
+  }
 
-    .date {
-        font-size: 30px;
-        font-weight: 600;
-        /*border: red solid 1px;*/
-    }
+  .schedules {
+    display: flex;
+    flex-direction: row;
+    /*border: aqua solid 1px;*/
+  }
 
-    .descs {
-        margin-left: 30px;
-        /*border: green solid 1px;*/
-    }
+  .date {
+    font-size: 30px;
+    font-weight: 600;
+    /*border: red solid 1px;*/
+  }
 
-    .desc {
-        margin-top: 10px
-        /*border: blue solid 1px;*/
-    }
+  .descs {
+    margin-left: 30px;
+    /*border: green solid 1px;*/
+  }
 
-    .date > p {
-        margin-top: 0
-    }
+  .desc {
+    margin-top: 10px
+    /*border: blue solid 1px;*/
+  }
+
+  .date > p {
+    margin-top: 0
+  }
 
 
 </style>
